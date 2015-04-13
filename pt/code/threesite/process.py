881bb6e57f207b946bf246fb2f43017e9ce77d1d
Nick <nick.russell@bristol.ac.uk>
2015-04-13 18:38:16 +0100

    Compute fidelities for 3-site data
    Extend processing script to generate fidelities
    Add table to LaTeX

--- pt/code/threesite/process.py
+++ pt/code/threesite/process.py
@@ -108,6 +108,19 @@ format(mode+1)
     pairs[i]/=sum(pairs[i])
 
   return singles,singles_err,pairs,pairs_err
+
+def IdealPairs(U,mode):
+  '''Get the pairs (normalised over visible pairs to unity) for matrix U
+and input modes mode
+U      : unitary
+mode   : input modes (i,j)
+return : (numpy array) 15 pairs normalised to 1'''
+  pairs=[(0,1),(0,2),(0,3),(0,4),(0,5),(1,2),(1,3),(1,4),(1,5),(2,3),(2,4),\
+(2,5),(3,4),(3,5),(4,5)]
+  powers = numpy.array([abs2(U[i,mode[0]]*U[j,mode[1]]+\
+U[i,mode[1]]*U[j,mode[0]]) for i,j in pairs])
+  powers /= sum(powers)
+  return powers
   
 if __name__=='__main__':
   s=(0,5.135)
@@ -121,6 +134,7 @@ if __name__=='__main__':
 format(i),'w') for i in range(6)]
   pairs_out=[open("data/threesite/symmetric/experimental/pairs{0:d}{1:d}.dat".\
 format(i,j),'w') for i,j in [(0,1),(1,2),(0,2)]]
+  fidelities_out=open("data/threesite/symmetric/fidelities.dat",'w')
   for i in range(6):
     singles_out[i].write("# gamma {0:.4f}\n# eta   {1:.4f}\n# t 0 1 2 3 4 5 d0\
  d1 d2 d3 d4 d5\n".format(gamma,eta))
@@ -128,6 +142,8 @@ format(i,j),'w') for i,j in [(0,1),(1,2),(0,2)]]
     pairs_out[i].write("# gamma {0:.4f}\n# eta   {1:.4f}\n# t 01 02 03 04 05\
  12 13 14 15 23 24 25 34 35 45 d01 d02 d03 d04 d05 d12 d13 d14 d15 d23 d24 d25\
  d34 d35 d45\n".format(gamma,eta))
+  fidelities_out.write("# gamma {0:.4f}\n# eta   {1:.4f}\n# t 0 1 2 3 4 5 01\
+ 12 02\n".format(gamma,eta))
   singles_fmt="{0:.6f}"
   for i in range(1,13):
     singles_fmt+=" {"+str(i)+":.6f}"
@@ -136,9 +152,22 @@ format(i,j),'w') for i,j in [(0,1),(1,2),(0,2)]]
   for i in range(1,31):
     pairs_fmt+=" {"+str(i)+":.6f}"
   pairs_fmt+="\n"
+  fidelities_fmt="{0:.6f}"
+  for i in range(1,10):
+    fidelities_fmt+=" {"+str(i)+":.6f}"
+  fidelities_fmt+="\n"
 
   for n,t in enumerate(symmetric_tree.points()):
+    U,norm=threesitematrix(gamma,eta,t)
     singles,singles_err,pairs,pairs_err=GetData("symmetric",n)
+    # Get fidelities
+    singles_fidelities=[1-0.5*sum(abs(numpy.array([abs2(U[i,j]) \
+for j in range(6)])-singles[i])) for i in range(6)]
+    pairs_ideal=[IdealPairs(U,mode) for mode in [(0,1),(1,2),(0,2)]]
+    pairs_fidelities=[1-0.5*sum(abs(pairs_ideal-pairs)) for i in range(3)]
+    fidelities_out.write(fidelities_fmt.format(t,*numpy.append(\
+singles_fidelities,pairs_fidelities)))
+    # Write experimental data
     for i in range(6):
       singles_out[i].write(singles_fmt.format(t,*numpy.append(singles[i],\
 singles_err[i])))
@@ -150,14 +179,16 @@ pairs_err[i])))
     singles_out[i].close()
   for i in range(3):
     pairs_out[i].close()
+  fidelities_out.close()
 
 ### BROKEN
   eta=1
-  gamma=0.5*numpy.sqrt(2)*eta
+  gamma=1.05*numpy.sqrt(2)*eta
   singles_out=[open("data/threesite/broken/experimental/singles{0:d}.dat".\
 format(i),'w') for i in range(6)]
   pairs_out=[open("data/threesite/broken/experimental/pairs{0:d}{1:d}.dat".\
 format(i,j),'w') for i,j in [(0,1),(1,2),(0,2)]]
+  fidelities_out=open("data/threesite/broken/fidelities.dat",'w')
   for i in range(6):
     singles_out[i].write("# gamma {0:.4f}\n# eta   {1:.4f}\n# t 0 1 2 3 4 5 d0\
  d1 d2 d3 d4 d5\n".format(gamma,eta))
@@ -165,6 +196,8 @@ format(i,j),'w') for i,j in [(0,1),(1,2),(0,2)]]
     pairs_out[i].write("# gamma {0:.4f}\n# eta   {1:.4f}\n# t 01 02 03 04 05\
  12 13 14 15 23 24 25 34 35 45 d01 d02 d03 d04 d05 d12 d13 d14 d15 d23 d24 d25\
  d34 d35 d45\n".format(gamma,eta))
+  fidelities_out.write("# gamma {0:.4f}\n# eta   {1:.4f}\n# t 0 1 2 3 4 5 01\
+ 12 02\n".format(gamma,eta))
   singles_fmt="{0:.6f}"
   for i in range(1,13):
     singles_fmt+=" {"+str(i)+":.6f}"
@@ -173,9 +206,22 @@ format(i,j),'w') for i,j in [(0,1),(1,2),(0,2)]]
   for i in range(1,31):
     pairs_fmt+=" {"+str(i)+":.6f}"
   pairs_fmt+="\n"
+  fidelities_fmt="{0:.6f}"
+  for i in range(1,10):
+    fidelities_fmt+=" {"+str(i)+":.6f}"
+  fidelities_fmt+="\n"
 
   for n,t in enumerate(broken_tree.points()):
+    U,norm=threesitematrix(gamma,eta,t)
     singles,singles_err,pairs,pairs_err=GetData("broken",n)
+    # Get fidelities
+    singles_fidelities=[1-0.5*sum(abs(numpy.array([abs2(U[i,j]) \
+for j in range(6)])-singles[i])) for i in range(6)]
+    pairs_ideal=[IdealPairs(U,mode) for mode in [(0,1),(1,2),(0,2)]]
+    pairs_fidelities=[1-0.5*sum(abs(pairs_ideal-pairs)) for i in range(3)]
+    fidelities_out.write(fidelities_fmt.format(t,*numpy.append(\
+singles_fidelities,pairs_fidelities)))
+    # Wrte experimental data
     for i in range(6):
       singles_out[i].write(singles_fmt.format(t,*numpy.append(singles[i],\
 singles_err[i])))
@@ -187,3 +233,28 @@ pairs_err[i])))
     singles_out[i].close()
   for i in range(3):
     pairs_out[i].close()
+  fidelities_out.close()
+
+  f_symmetric=numpy.loadtxt("data/threesite/symmetric/fidelities.dat")
+  fout=open("data/threesite/symmetric/fidelities.dat",'a')
+  fout.write("\n")
+  fout.write("# singles:  {0:.4f}+-{1:.4f}\n".format(\
+numpy.mean(f_symmetric[:,1:7]),\
+numpy.std(f_symmetric[:,1:7])))
+  fout.write("# twofolds: {0:.4f}+-{1:.4f}\n".format(\
+numpy.mean(f_symmetric[:,8:11]),\
+numpy.std(f_symmetric[:,8:11])))
+  fout.close()
+
+  f_broken=numpy.loadtxt("data/threesite/broken/fidelities.dat")
+  fout=open("data/threesite/broken/fidelities.dat",'a')
+  fout.write("\n")
+  fout.write("# singles:  {0:.4f}+-{1:.4f}\n".format(\
+numpy.mean(f_broken[:,1:7]),\
+numpy.std(f_broken[:,1:7])))
+  fout.write("# twofolds: {0:.4f}+-{1:.4f}\n".format(\
+numpy.mean(f_broken[:,8:11]),\
+numpy.std(f_broken[:,8:11])))
+  fout.close()
+
+  
